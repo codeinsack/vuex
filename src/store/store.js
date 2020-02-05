6e7c00c1ed15537bccb7464e76f6b4bd20a693db
codeinsack <codeinsack@gmail.com>
2020-02-05 12:26:18 +0300

    mapping getters

--- src/store/store.js
+++ src/store/store.js
@@ -11,5 +11,8 @@ export const store = new Vuex.Store({
     doubleCounter: state => {
       return state.counter * 2
     },
+    stringCounter: state => {
+      return state.counter + ' Clicks'
+    }
   },
 })
